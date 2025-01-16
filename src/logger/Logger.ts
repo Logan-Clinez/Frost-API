68cb8813f95f4e2a9437f23daf3e5abf153bcf50
Logan <kenny@bytenodes.xyz>
2025-01-16 12:55:17 -0500

    Update Logger.ts

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -68,7 +68,7 @@ export default class RCELogger implements ILogger {
       const date = new Date();
       const timestamp = date.toLocaleTimeString([], { hour12: true });
       const padding = " ".repeat(Math.max(0, 15 - logType.prefix.length));
-      const formattedMessage = `${ConsoleColor.FgCyan}${ConsoleColor.Bold}[${timestamp}]${ConsoleColor.Reset} ${logType.color}${logType.prefix} ${padding}${logType.emoji}${ConsoleColor.Reset}`;
+      const formattedMessage = `${ConsoleColor.FgCyan}${ConsoleColor.Bold}[${timestamp}]${ConsoleColor.Reset} ${logType.color}${logType.prefix} ${padding}${logType.emoji} ${ConsoleColor.Reset}`;
 
       console.log(formattedMessage, this.format(message));
     }
